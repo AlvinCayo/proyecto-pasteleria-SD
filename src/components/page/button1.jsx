ea0d36dba38f538c13c6464bf2ae4526b1eef355
Derix Venegas <derix.dv19@gmail.com>
2023-11-17 09:24:01 -0400

    componentes

---
+++ src/components/page/button1.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./styles/button1.css"
+
+export const Button1 = () => {
+  return (
+    <button className='btn1'>Ingresar como invitado</button>
+  )
+}
---
+++ src/components/page/button3.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./styles/button3.css"
+
+export const Button3 = () => {
+  return (
+    <button className='btn3'>Iniciar Sesión</button>
+  )
+}
