c604e9fb77eb7e9a48e35cf634d4c063113900f4
Alvin Cayo <cayoalvin991@gmail.com>
2023-11-17 00:38:33 -0400

    creando botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import { Pantalla1 } from './components/pantalla1'
 import { Card1 } from './components/page/card1'
 
 function App() {
--- src/components/page/card1.jsx
+++ src/components/page/card1.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import "./styles/card1.css";
+import "./card1.css";
 import { Button1 } from "./button1";
+import { Button2 } from "./button2";
 
 export const Card1 = () => {
   return (
-    <div>
+    <div className="p1">
       <img src="./public/pasteleria-san-diego.jpg" alt="imagen" />
+      <Button1></Button1>
+      <Button2></Button2>
     </div>
   );
 };
