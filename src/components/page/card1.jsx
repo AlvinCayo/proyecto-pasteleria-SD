e91c43dfc5b9d2de16a90b42eb1fc946c3bd839f
Alvin Cayo <cayoalvin991@gmail.com>
2023-11-16 23:44:31 -0400

    creacion de la primera pagina

---
+++ src/components/page/card1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/card1.css";
+import { Button1 } from "./button1";
+
+export const Card1 = () => {
+  return (
+    <div>
+      <img src="./public/pasteleria-san-diego.jpg" alt="imagen" />
+    </div>
+  );
+};
