7f1dc0ac61b59c13c6ef2142bb58f9342a7c26d6
Darkweaver <albasantiagoe@hotmail.com>
2023-11-22 19:57:05 -0400

    React

---
+++ src/components/rectangulo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/rectangulo.css";
+
+function rectangulo() {
+  return (
+    <section>
+      <div className="Rectangulo"></div>
+      <h1>hola derix</h1>
+    </section>
+  );
+}
+export default rectangulo;
