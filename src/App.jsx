1ab20b2f6eedcb38d0faa82930ddb93084d350b4
Derix Venegas <derix.dv19@gmail.com>
2023-11-23 10:04:40 -0400

    Notificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-import Back from './components/Back'
-import Logo from './components/Logo'
-import Welcome from './components/Welcome'
-import Datos from './components/Datos'
-import Registrar from './components/Registrar'
-import Ac_pol from './components/Ac_pol'
-import Entra from './components/Entra'
+import Registro from './components/Registro'
+import Notificaciones from './components/Notificaciones'
 
 function App() {
 
   return (
     <>
-      <Back/>
-      <Logo/>
-      <Welcome/>
-      <Datos/>
-      <Ac_pol/>
-      <Registrar/>
-      <Entra/>
+    <div>
+      <Notificaciones/>
+    </div>
+    <div>
+      <Registro/>
+    </div>
     </>
   )
 }
