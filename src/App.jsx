e66e797ef55e8c716db319b58d9ef3d34c68a646
Darkweaver <albasantiagoe@hotmail.com>
2023-11-23 10:20:07 -0400

    semifinal

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from "react";
 import "./App.css";
 import Rectangulo from "./components/Rectangulo";
 import Boton from "./components/Boton";
+import RectanguloV2 from "./components/RectanguloV2";
+import Estrella from "./components/Estrella";
 
 function App() {
   return (
     <>
-      <Rectangulo />
-      <Boton />
+      <div>
+        <Rectangulo />
+      </div>
+      <div>
+        <Boton />
+      </div>
+      <div>
+        <RectanguloV2 />
+        <Estrella />
+      </div>
     </>
   );
 }
---
+++ src/components/Estrella.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/Estrella.css";
+
+function Estrella() {
+  return (
+    <section className="seccion2">
+      <div>
+        <div>
+          <p>
+            Acumula estrellas. Con Pastelería San Diego® Rewards, cada 20 Bs.
+            gastado en tus compras, automaticamente se convertirá en 100
+            estrellas en tu app. Tantas estrellas 🌟 que necesitarás tus 🕶️.
+            3.000 Estrellas Cada 3.000 estrellas acumuladas te regalaremos tu
+            cupcake favorito. 🧁 ¡Tus días brillaran más! 10.000 Estrellas Al
+            conseguir 10.000 estrellas en 12 meses te regalaremos tu pastel
+            favorito. 🎂{" "}
+          </p>
+        </div>
+        <div className="acumuladas">
+          <h2>Estrellas Acumuladas </h2>
+          <h2>Total _______________ 100 🌟</h2>
+          <button className="estrellaboton">Ganar Estrellas</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Estrella;
