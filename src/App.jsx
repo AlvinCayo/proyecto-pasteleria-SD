583fc7921d535cdfd4ec7bb61398d91d8d3c610b
Lisandro1072 <lisandrotintaya@gmail.com>
2023-11-23 07:05:17 -0400

    local

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
-import "./App.css";
-import { Card2 } from "./components/page2/card2.css";
+import { useState } from 'react'
+import './App.css'
+import { Card2 } from './components/pages/localitation/card2'
+import { Burb } from './components/atoms/burb'
 
 function App() {
   return (
     <>
       <div className="rectangulo">
-        <Card2></Card2>
+        <img src="map.png" alt=""></img>
+        <p className="h">0h 15m</p>
+        <p className="d">El delivery llegara</p>
+        <div className="rect">
+          <p className="t">SN-3836</p>
+          <Card2></Card2>
+          <Burb></Burb>
+        </div>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
