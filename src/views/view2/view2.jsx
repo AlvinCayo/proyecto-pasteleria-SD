78984380a8dbf3172400c3abc22a14c4a060b814
Alvin Cayo <cayoalvin991@gmail.com>
2023-11-23 16:10:56 -0400

    view2 casi listo

--- src/views/view2/view2.jsx
+++ src/views/view2/view2.jsx
@@ -5,22 +5,28 @@ import buttonData2 from "../../components/button2/buttonData2";
 import { Tittle } from "../../components/tittle/tittle";
 import { BackButton } from "../../components/backButton/backButton";
 import { Buscador } from "../../components/buscador/buscador";
+import { IncomeD } from "../../components/incomeData/incomeD";
+import { ButtonE } from "../../components/buttonEntry/buttonE";
+import { Tittle2 } from "../../components/tittle2/tittle2";
 
 export const View2 = () => {
   return (
     <div className="p2">
-      <Tittle />
-      <BackButton />
-      <Buscador letras="Ingresa tu Correo aquí" />
-      <Buscador letras="Ingresa tu contraseña aquí" />
-      <Button2
-        name={"Ingresar"}
-        backgroundColor={"C02529"}
-        textColor={"FFFFFF"}
-        fontSize={"16px"}
-        fontFamily={"Poppins"}
-      />
-      <div className="containerButtons2">
+      <div className="arriba">
+        <Tittle />
+        <BackButton />
+      </div>
+
+      <div className="Dingreso">
+        <IncomeD letras="Ingresa tu Correo" />
+        <IncomeD letras="Ingresa tu contraseña" />
+        <ButtonE />
+      </div>
+
+      <div className="tittle2">
+        <Tittle2 titulo="Bienvenido Inicia tu Sesión!" />
+      </div>
+      <div className="containerButtons2"> 
         {buttonData2.map((button, index) => (
           <Button2
             key={index}
