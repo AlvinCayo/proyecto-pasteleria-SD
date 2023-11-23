c83fa3224c05376a2eee50ceec4e421817be3218
Alvin Cayo <cayoalvin991@gmail.com>
2023-11-22 23:25:38 -0400

    agregando diseño

---
+++ src/components/backButton/backButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BackButton = () => {
+  return (
+    <img src="./public/Back Button.svg" alt="icono" />
+  )
+}
--- src/views/view2/view2.jsx
+++ src/views/view2/view2.jsx
@@ -3,11 +3,13 @@ import "./view2.css";
 import { Button2 } from "../../components/button2/button2";
 import buttonData2 from "../../components/button2/buttonData2";
 import { Tittle } from "../../components/tittle/tittle";
+import { BackButton } from "../../components/backButton/backButton";
 
 export const View2 = () => {
   return (
     <div className="p2">
       <Tittle/>
+      <BackButton className="bButton"/>
       <div className="containerButtons2">
         {buttonData2.map((button, index) => (
           <Button2
