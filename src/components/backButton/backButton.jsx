1296ff961079e909b766cff103f049b1e43dc1b6
Derix Venegas <derix.dv19@gmail.com>
2023-11-23 17:52:11 -0400

    vamos

---
+++ src/components/backButton/backButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './backButton.css'
+
+export const BackButton = () => {
+  return (
+    <button className="bButton">
+      <img src="./public/Back Button.svg" alt="icono" />
+    </button>
+  );
+};
---
+++ src/views/view2/view2.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./view2.css";
+import { Button2 } from "../../components/button2/button2";
+import buttonData2 from "../../components/button2/buttonData2";
+import { Tittle } from "../../components/tittle/tittle";
+import { BackButton } from "../../components/backButton/backButton";
+
+export const View2 = () => {
+  return (
+    <div className="p2">
+      <Tittle/>
+      <BackButton/>
+      <div className="containerButtons2">
+        {buttonData2.map((button, index) => (
+          <Button2
+            key={index}
+            name={button.name}
+            backgroundColor={button.backgroundColor}
+            textColor={button.textColor}
+            fontSize={button.fontSize}
+            fontFamily={button.fontFamily}
+            svg={button.svg}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
