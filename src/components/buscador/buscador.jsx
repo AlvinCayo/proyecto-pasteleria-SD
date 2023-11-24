55198357a42781e94d0343eb208ca296660ebbde
Alvin Cayo <cayoalvin991@gmail.com>
2023-11-24 08:21:19 -0400

    arreglando buscador

--- src/components/buscador/buscador.jsx
+++ src/components/buscador/buscador.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import './buscador.css'
+import React from "react";
+import "./buscador.css";
 
 export const Buscador = ({ letras }) => {
   return (
-    <div className='buscador'>
-        <div className='letrasb'>{letras}</div>
-  )
-}
+    <div className="buscador">
+      <div className="letrasb">{letras}</div>
+    </div>
+  );
+};
