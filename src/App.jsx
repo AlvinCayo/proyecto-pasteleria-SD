e071ff97063c10b9366eda9614ebeaa35b926d41
Derix Venegas <derix.dv19@gmail.com>
2023-11-24 10:16:08 -0400

    ah

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import { Location } from './views/location/location'
-import { Card } from './views/shopping/card'
-import './App.css'
-import { View1 } from './views/view1/view1'
-import { View2 } from './views/view2/view2'
-import { Home } from './views/home/home'
-import { Producto } from './views/producto/producto'
-import { View3 } from './views/view3/view3'
-import { Viewpay } from './views/viewpay/viewpay'
+import { useState } from "react";
+import { Location } from "./views/location/location";
+import { Card } from "./views/shopping/card";
+import "./App.css";
+import { View1 } from "./views/view1/view1";
+import { View2 } from "./views/view2/view2";
+import { Home } from "./views/home/home";
+import { Producto } from "./views/producto/producto";
+import { View3 } from "./views/view3/view3";
+import { Viewpay } from "./views/viewpay/viewpay";
 
 function App() {
   return (
     <>
       <div>
-      <Card></Card>
-      </div>/
+        <Card></Card>
+      </div>
+      /
     </>
-  )
+  );
 }
 
-export default App
+export default App;
