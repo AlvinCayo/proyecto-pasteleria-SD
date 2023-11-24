f02293b4da3063b0498c757fd0df71816e165abd
Derix Venegas <derix.dv19@gmail.com>
2023-11-24 07:55:40 -0400

    Componente Stock

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import './App.css'
 import { View1 } from './views/view1/view1'
 import { View2 } from './views/view2/view2'
 import { Home } from './views/home/home'
-import { Registro } from './views/registro/registro'
+import { Producto } from './views/producto/producto'
 
 function App() {
 
   return (
     <>
       <div>
-        <Registro/>
+        <Producto/>
       </div>
     </>
   )
---
+++ src/views/producto/producto.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './producto.css'
+import buttonCarrito from '../../components/button2/buttonCarrito'
+import { BackButton } from '../../components/backButton/backButton'
+import { Button } from '../../components/button/button'
+import { Stock } from '../../components/stock/stock'
+
+export const Producto = () => {
+  return (
+    <div className="p3">
+      <div className="inicio">
+        <BackButton/>
+        <img src="alfajor.png" alt="alfajor" className='alfajor' />
+      </div>
+      <div>
+        <h3 className='alfajores'>Alfajores</h3>
+      </div>
+      <div>
+        <h3 className='disp'>Stock:</h3>
+        <Stock enStock={15}/>
+      </div>
+      <div className="containerButtonCarrito">
+        {buttonCarrito.map((button, index) => (
+            <Button
+                key={index}
+                name={button.name}
+                backgroundColor={button.backgroundColor}
+                textColor={button.textColor}
+                fontSize={button.fontSize}
+                fontFamily={button.fontFamily}
+            />
+        ))}
+      </div>
+      <div>
+        <h4 className='desProducto'>Suaves y mantecosas, a menudo con una textura similar a la masa de pastelería. El dulce de leche, que es una mezcla caramelizada de leche y azúcar, es el componente central que le proporciona su sabor característico y suavidad. Después de unir las capas, el alfajor puede ser cubierto con chocolate, espolvoreado con coco rallado o glaseado con azúcar, lo que agrega una capa adicional de sabor y textura.</h4>
+      </div>
+    </div>
+  )
+}
