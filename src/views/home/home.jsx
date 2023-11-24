72736f0ea1815f2149d8da530801f545639f5256
Alvin Cayo <cayoalvin991@gmail.com>
2023-11-24 11:28:31 -0400

    arreglando home

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-import React from "react";
-import "./home.css";
-import { Buscador } from "../../components/buscador/buscador";
-import { NamePage } from "../../components/namePage/namePage";
-import { Options } from "../../components/options/options";
-<<<<<<< HEAD
-=======
 import React from 'react'
 import './home.css'
 import { Buscador } from '../../components/buscador/buscador'
 import { NamePage } from '../../components/namePage/namePage'
 import { Options } from '../../components/options/options'
->>>>>>> Derix
-=======
 import { ButtonLupa } from "../../components/buttonLupa/buttonLupa";
 import { Categori } from "../../components/categori/categori";
 import { Product } from "../../components/product/product";
 import svgPaths from "../../components/categori/svgPaths";
 import { products } from "../../components/product/productData";
->>>>>>> 0158e48b4e8c5ba427c8ee183198c425f3dcd3a6
 
 export const Home = () => {
   return (
@@ -30,19 +19,6 @@ export const Home = () => {
           <ButtonLupa />
         </div>
       </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <Options/>
-    </div>
-  );
-};
-=======
-      <Options />
-    </div>
-  )
-}
->>>>>>> Derix
-=======
       <div className="categorias">
         {svgPaths.map((item, index) => (
           <Categori key={index} svg={item.path} name={item.nombre} />
@@ -57,4 +33,3 @@ export const Home = () => {
     </div>
   )
 }
->>>>>>> 0158e48b4e8c5ba427c8ee183198c425f3dcd3a6
